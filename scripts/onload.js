1d458b7394ecbf834940363d8bfb4e58027edcde
PeterYang1625 <peteryang1625@gmail.com>
2016-01-25 00:04:38 -0800

    fixed load bug

--- scripts/onload.js
+++ scripts/onload.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
+    function loady(){
     $("div.hidden")
         .css('opacity', 0)
         .slideDown(1000)
         .animate(
             { opacity: 1 },
             { queue: false, duration: 1500 }
-  );
+  );}
+    setTimeout(loady, 100);
 });
