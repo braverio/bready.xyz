06260db8417584cebc05131740ef0d55c319f8bb
Peter Yang <peteryang1625@gmail.com>
2016-01-28 16:24:21 -0800

    moar front paeg

--- contact.php
+++ contact.php
@@ -5,6 +5,8 @@ $subject = $_POST["subject"];
 $message = $_POST["message"];
 
 $message = "Message from $from ($email):\n\n" . $message;
-mail ("peteryang1625+bready@gmail.com", $subject, $message);
+if (filter_var($email, FILTER_VALIDATE_EMAIL)){
+    mail ("peteryang1625+bready@gmail.com", $subject, $message);
+}
 header('Location: https://bready.xyz');
 ?>
