c55d7de81629a68684036ce9e2c134fe69f652eb
Kevin Tran <me@kevintran.codes>
2016-02-14 00:49:54 -0800

    Added nonce field to contact.

--- contact/contact.php
+++ contact/contact.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 $from = $_POST["from"];
 $email = $_POST["email"];
 $subject = $_POST["subject"];
@@ -6,10 +7,12 @@ $message = $_POST["message"];
 
 $message = "Message from $from ($email):\n\n" . $message;
 $location = "Location: http://bready.xyz/contact/index.php?text=";
-if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+if (!isset($_SESSION['hash']) || !isset($_POST['token']) || $_SESSION['hash'] != $_POST['token']) {
+	header($location . "An error occured");
+} else if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
     mail ("peteryang1625+bready@gmail.com", $subject, $message);
     header($location . "Sent!");
-}
+} 
 else{
     header($location . "Please enter a valid email address");
 }
